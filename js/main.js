3e78e5f974f6f57aa3c115ab6183d5449dd79ac8
Runi Goswami <runigoswami@gmail.com>
2015-07-06 15:57:10 -0400

    no more snap

--- js/main.js
+++ js/main.js
@@ -79,56 +79,42 @@
   // };
 
 
-  var s = Snap('#runicloud_svg');
-  Snap.load("img/svgs/runicloud.svg", function(f) {
-    // Note that we traversre and change attr before SVG
-    // is even added to the page
-    var thoughts = [
-      "#brain", "#soundcloud", "#deathly_hallows", "#paintbrush", "#om", "#skate", "#wave", "#tea",
-      "#mantis_shrimp", "#photos", "#tjhsst", "#play_buttons", "#oxytocin", "#dodecahedron", "#plane_ticket"
-    ];
-
-    s.append(f);
-    for (i = 0; i < thoughts.length; i++) {
-      // console.log(thoughts[i]+y);
-      var thought = thoughts[i];
-      s.select(thought + "_container").hover(
-        function(d) {
-          var sel = '#' + d.path[1].id.replace("_container", "");;
-          console.log(sel);
-          s.select(sel).attr({
-            fill: "#5acecf"
-          });
-        },
-        function(d) {
-          var sel = '#' + d.path[1].id.replace("_container", "");;
-          console.log(sel);
-          s.select(sel).attr({
-            fill: "#434343"
-          });
-        }
-      );
-    }
-    // var brain = s.select(thoughts[0]+"_container");
-    // brain.hover(
-    //   function(d) {
-    //     s.select(thoughts[0]).attr({
-    //       fill: "#5acecf"
-    //     });
-    //   },
-    //   function(d) {
-    //     s.select(thoughts[0]).attr({
-    //       fill: "#434343"
-    //     });
-    //   }
-    // );
-
-
-
-    s.select('svg').attr({
-      preserveAspectRatio: "xMaxYMin meet"
-    });
-  });
+  // var s = Snap('#runicloud_svg');
+  // Snap.load("img/svgs/runicloud.svg", function(f) {
+  //   // Note that we traversre and change attr before SVG
+  //   // is even added to the page
+  //   var thoughts = [
+  //     "#brain", "#soundcloud", "#deathly_hallows", "#paintbrush", "#om", "#skate", "#wave", "#tea",
+  //     "#mantis_shrimp", "#photos", "#tjhsst", "#play_buttons", "#oxytocin", "#dodecahedron", "#plane_ticket"
+  //   ];
+  //
+  //   s.append(f);
+  //   for (i = 0; i < thoughts.length; i++) {
+  //     // console.log(thoughts[i]+y);
+  //     var thought = thoughts[i];
+  //     s.select(thought + "_container").hover(
+  //       function(d) {
+  //         var sel = '#' + d.path[1].id.replace("_container", "");;
+  //         console.log(sel);
+  //         s.select(sel).attr({
+  //           fill: "#5acecf"
+  //         });
+  //       },
+  //       function(d) {
+  //         var sel = '#' + d.path[1].id.replace("_container", "");;
+  //         console.log(sel);
+  //         s.select(sel).attr({
+  //           fill: "#434343"
+  //         });
+  //       }
+  //     );
+  //   }
+  //
+  //
+  //   s.select('svg').attr({
+  //     preserveAspectRatio: "xMaxYMin meet"
+  //   });
+  // });
 
 
 
