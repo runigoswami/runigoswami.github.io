9fad1f1860963314595a721fc388b259857b46d4
Runi Goswami <runigoswami@gmail.com>
2015-07-17 18:36:03 -0400

    trying to disable skrollr for phones

--- js/main.js
+++ js/main.js
@@ -10,28 +10,29 @@
     }, 1);
   });
 
-  function adjustWindow() {
-    var s = skrollr.init({
-      render: function(data) {},
-      smoothScrolling: true
-    });
-    winH = $window.height();
-    if (winH <= 450) {
-      winH = 450;
-    }
-    $slide.height(winH);
-    var halfWindowHeight = winH - (winH / 2);
-    var onethirdsHeight = (winH) / 3;
-    $('#cover-1').height(2 * onethirdsHeight);
-    $('#works').height(.9 * winH);
-    s.refresh($('.cover, .content'));
-  }
-
   if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
-    skrollr.init({
-      forceHeight: false
-    });
+    function adjustWindow() {
+      var s = skrollr.init({
+        render: function(data) {},
+        smoothScrolling: true
+      });
+      winH = $window.height();
+      if (winH <= 450) {
+        winH = 450;
+      }
+      $slide.height(winH);
+      var halfWindowHeight = winH - (winH / 2);
+      var onethirdsHeight = (winH) / 3;
+      $('#cover-1').height(2 * onethirdsHeight);
+      $('#works').height(.9 * winH);
+      s.refresh($('.cover, .content'));
+    }
   }
+  // if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+  //   skrollr.init({
+  //     forceHeight: false
+  //   });
+  // }
   causeRepaintsOn = $("h1, h2, h3, p");
   $(window).resize(function() {
     causeRepaintsOn.css("z-index", 1);
