9c46f3dc219d23d22ac9c2a1d69919f5e489e679
Tim Tregubov <tim@zingweb.com>
2015-07-23 16:31:59 -0400

    some speedups

--- js/main.js
+++ js/main.js
@@ -1,82 +1,110 @@
 (function($) {
+
   $window = $(window);
   $slide = $('.cover, .content');
   $body = $('body');
 
-  $body.imagesLoaded(function() {
-    setTimeout(function() {
-      adjustWindow();
-      $body.removeClass('loading').addClass('loaded');
-    }, 1);
-  });
+  causeRepaintsOn = $("h1, h2, h3, p");
 
-  if (!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
-    function adjustWindow() {
-      var s = skrollr.init({
-        render: function(data) {},
-        smoothScrolling: true
-      });
-      winH = $window.height();
-      if (winH <= 450) {
-        winH = 450;
+  var snap = Snap('#runicloud_svg');
+  if (snap) {
+    Snap.load("/img/svgs/runicloud.svg", function(f) {
+      var thoughts = ["#brain", "#soundcloud", "#deathly_hallows", "#paintbrush", "#om", "#skate", "#wave", "#tea", "#mantis_shrimp", "#photos", "#tjhsst", "#play_buttons", "#oxytocin", "#dodecahedron", "#plane_ticket"];
+      snap.append(f);
+      for (i = 0; i < thoughts.length; i++) {
+        var thought = thoughts[i];
+        snap.select(thought + "_container").hover(function(d) {
+          var sel = '#' + d.path[1].id.replace("_container", "");;
+          console.log(sel);
+          snap.select(sel).attr({
+            fill: "#5acecf"
+          });
+        }, function(d) {
+          var sel = '#' + d.path[1].id.replace("_container", "");;
+          console.log(sel);
+          snap.select(sel).attr({
+            fill: "#434343"
+          });
+        });
       }
-      $slide.height(winH);
-      var halfWindowHeight = winH - (winH / 2);
-      var onethirdsHeight = (winH) / 3;
-      // $('#cover-1').height(2 * onethirdsHeight);
-      $('#works').height(.9 * winH);
-      // $('#content-0').height(1.6 * winH);
-      s.refresh($('.cover, .content'));
-    }
+      snap.select('svg').attr({
+        preserveAspectRatio: "xMaxYMin meet"
+      });
+    });
   }
 
-  causeRepaintsOn = $("h1, h2, h3, p");
-  $(window).resize(function() {
-    causeRepaintsOn.css("z-index", 1);
-    adjustWindow();
+  var s = skrollr.init({
+    smoothScrolling: true,
+    forceHeight: false
   });
-  var s = Snap('#runicloud_svg');
-  Snap.load("/img/svgs/runicloud.svg", function(f) {
-    var thoughts = ["#brain", "#soundcloud", "#deathly_hallows", "#paintbrush", "#om", "#skate", "#wave", "#tea", "#mantis_shrimp", "#photos", "#tjhsst", "#play_buttons", "#oxytocin", "#dodecahedron", "#plane_ticket"];
-    s.append(f);
-    for (i = 0; i < thoughts.length; i++) {
-      var thought = thoughts[i];
-      s.select(thought + "_container").hover(function(d) {
-        var sel = '#' + d.path[1].id.replace("_container", "");;
-        console.log(sel);
-        s.select(sel).attr({
-          fill: "#5acecf"
-        });
-      }, function(d) {
-        var sel = '#' + d.path[1].id.replace("_container", "");;
-        console.log(sel);
-        s.select(sel).attr({
-          fill: "#434343"
-        });
+
+
+  //  triggers an adjust once everything is fully loaded
+  // without this skrollr is jerky
+  $window.load(function() {
+    $window.trigger('resize');
+    var s = skrollr.init({
+      smoothScrolling: true,
+      forceHeight: false
+    });
+
+  });
+
+  //fades in once images are all loaded
+  $body.imagesLoaded(function() {
+    setTimeout(function() {
+      $body.removeClass('loading').addClass('loaded');
+      adjustWindow();
+    }, 100);
+  });
+
+
+  // adjusts various heights of things and skrollr refresh
+  var adjustWindow = function() {
+    console.log('adjusting window');
+
+    winH = $window.height();
+    if (winH <= 450) {
+      winH = 450;
+    }
+    $slide.height(winH);
+    var halfWindowHeight = winH - (winH / 2);
+    var onethirdsHeight = (winH) / 3;
+    // $('#cover-1').height(2 * onethirdsHeight);
+    $('#works').height(.9 * winH);
+    // $('#content-0').height(1.6 * winH);
+    //s.refresh($('.cover, .content'));
+    s.refresh();
+  }
+
+
+  $window.resize(function() {
+    causeRepaintsOn.css("z-index", 1);
+
+    if (!typeof $container === undefined) {
+      $container.isotope({
+        masonry: {
+          columnWidth: $container.width() / 10
+        }
       });
     }
-    s.select('svg').attr({
-      preserveAspectRatio: "xMaxYMin meet"
-    });
+    adjustWindow();
+
   });
-  $(document).ready(function() {
-    $(window).bind('scroll', function() {
-      var navHeight = $(window).height() - 70;
-      if ($(window).scrollTop() > navHeight) {
-        $('#filtration_bar').addClass('fixed');
-      } else {
-        $('#filtration_bar').removeClass('fixed');
-      }
-    });
+
+
+  $window.bind('scroll', function() {
+    var navHeight = $window.height() - 70;
+    if ($window.scrollTop() > navHeight) {
+      $('#filtration_bar').addClass('fixed');
+    } else {
+      $('#filtration_bar').removeClass('fixed');
+    }
   });
+
+
   $(document).ready(function() {
-    // var $container = $('#container')
-    // $container.isotope({
-    //   resizable: false,
-    //   masonry: {
-    //     columnWidth: $container.width() / 10
-    //   }
-    // });
+
     $('.filters-button-group').on('click', 'button', function() {
       var filterValue = $(this).attr('data-filter');
       $container.isotope({
@@ -89,13 +117,6 @@
       $buttonGroup.on('click', 'button', function() {
         $buttonGroup.find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
-      });
-    });
-    $(window).resize(function() {
-      $container.isotope({
-        masonry: {
-          columnWidth: $container.width() / 10
-        }
       });
     });
   });
